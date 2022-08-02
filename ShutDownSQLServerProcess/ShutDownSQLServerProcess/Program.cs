1bef854b585b31cfbad153af289c12930ffb3f79
Evaristo <ejmcapo@gmail.com>
2022-08-02 16:58:19 -0400

    Upload c# program
    Create c# main program code in github cloud

---
+++ ShutDownSQLServerProcess/ShutDownSQLServerProcess/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace ShutDownSQLServerProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
